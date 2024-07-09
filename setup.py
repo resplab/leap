2afd2a1d04c0f95ab4352367b88a218392c882c9
Ainsleigh <ainsleighhill@gmail.com>
2024-07-09 14:11:23 -0700

    create repo

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="leap",
+    version="1.0",
+    description="Lifetime Exposures and Asthma outcomes Projection (LEAP)",
+    author="Tae Yoon (Harry) Lee, Ainsleigh Hill",
+    author_email="ainsleighhill@gmail.com",
+    packages=["leap"],
+    install_requires=[
+      line.strip() for line in open("requirements.txt")
+    ],
+    extras_require={
+        "dev": ["pytest", "flake8"]
+    }
+)
