cf13c37bdd14964918bf81adee77d3ea1a4956e5
Ainsleigh <ainsleighhill@gmail.com>
2024-07-11 14:59:44 -0700

    add FamilyHistory class

---
+++ leap/family_history.py
@@ -0,0 +1,41 @@
+import numpy as np
+from leap.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class FamilyHistory:
+    """
+    A class containing information about family history of asthma.
+
+    Attributes:
+        parameters (dict): A dictionary containing the following keys:
+            * ``p``: float, the probability that an agent has a family history of asthma.
+    """
+    def __init__(self, config: dict | None = None, parameters: dict | None = None):
+        if config is None and parameters is None:
+            raise ValueError("Either config dict or parameters must be provided.")
+        elif config is not None:
+            self.parameters = config["parameters"]
+        else:
+            self.parameters = parameters
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, parameters):
+        if parameters["p"] > 1 or parameters["p"] < 0:
+            raise ValueError(
+                f"p must be a probability between 0 and 1, received {parameters['p']}."
+            )
+        self._parameters = parameters
+
+    def has_family_history_of_asthma(self) -> bool:
+        """Use Bernoulli distribution to determine whether an agent has a family history of asthma.
+
+        Returns:
+            bool: Whether or not an agent has a family history of asthma.
+        """
+        return bool(np.random.binomial(1, self.parameters["p"]))
