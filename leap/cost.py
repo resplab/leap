7fdbd95927f34d8b8c7ce9864ccc4c8fc7537b30
Ainsleigh <ainsleighhill@gmail.com>
2024-07-11 13:24:39 -0700

    add AsthmaCost class

---
+++ leap/cost.py
@@ -0,0 +1,52 @@
+import numpy as np
+from leap.control import ControlLevels
+from leap.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class AsthmaCost:
+    """
+    A class containing information about the cost of asthma.
+
+    Attributes:
+        parameters (dict): A dictionary containing the following keys:
+            * ``control``: A vector of numbers.
+            * ``exacerbation``: A vector of numbers.
+
+    """
+    def __init__(self, config: dict | None = None, parameters: dict | None = None):
+        if config is None and parameters is None:
+            raise ValueError("Either config dict or parameters must be provided.")
+        elif config is not None:
+            self.parameters = config["parameters"]
+            exchange_rate_usd_cad = config["exchange_rate_usd_cad"]
+            self.parameters["exac"] = np.array(self.parameters["exac"]) * exchange_rate_usd_cad
+            self.parameters["control"] = np.array(
+                self.parameters["control"]
+            ) * exchange_rate_usd_cad
+
+    def compute_cost(self, agent) -> float:
+        """Compute the cost in dollars for the current year due to asthma exacerbations and control.
+
+        If ``control_levels`` are not present, will default to equal probability of
+        fully controlled, partially controlled, and uncontrolled asthma.
+
+        Args:
+            agent (Agent): A person in the model.
+        """
+
+        if not agent.has_asthma:
+            return 0.0
+        else:
+            control_levels = agent.control_levels
+            if control_levels is None:
+                control_levels = ControlLevels(0.3333, 0.3333, 0.3333)
+                logger.warn(f"Control levels have not been set, default to {control_levels}")
+            return (
+                np.dot(
+                    agent.exacerbation_severity_hist.current_year,
+                    self.parameters["exacerbation"]
+                ) +
+                np.dot(control_levels.as_array(), self.parameters["control"])
+            )
