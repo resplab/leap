c7bd49b7fd77b25517d89efa979b92a9d2aba8ab
Ainsleigh <ainsleighhill@gmail.com>
2024-07-10 20:00:51 -0700

    add Control and ControlLevels classes

---
+++ leap/control.py
@@ -0,0 +1,122 @@
+import numpy as np
+from utils import compute_ordinal_regression
+
+
+class ControlLevels:
+    """A class containing the probability of each control level.
+
+    Attributes:
+        fully_controlled (float): The probability of being fully controlled.
+        partially_controlled (float): The probability of being partially controlled.
+        uncontrolled (float): The probability of being uncontrolled.
+    """
+    def __init__(
+        self, fully_controlled: float, partially_controlled: float, uncontrolled: float
+    ):
+        self.fully_controlled = fully_controlled
+        self.partially_controlled = partially_controlled
+        self.uncontrolled = uncontrolled
+
+    def as_array(self):
+        return np.array([self.fully_controlled, self.partially_controlled, self.uncontrolled])
+
+
+class Control:
+    """A class containing information about asthma control.
+
+    This refers to how well the condition is managed.
+    There are three levels of asthma control:
+        fully-controlled = 1
+        partially-controlled = 2
+        uncontrolled = 3
+
+    Attributes:
+        hyperparameters (dict): A dictionary containing the hyperparameters used
+            to compute `β0` from a normal distribution:
+            * ``β0_μ``: float, the mean of the normal distribution.
+            * ``β0_σ``: float, the standard deviation of the normal distribution.
+        parameters (dict): A dictionary containing the following keys:
+            * ``β0``: float, a constant parameter. See `hyperparameters`.
+            * ``βage``: float, the parameter for the age term.
+            * ``βsex``: float, the parameter for the sex term.
+            * ``βsexage``: float, the parameter for the sex * age term.
+            * ``βsexage2``: float, the parameter for the sex * age^2 term.
+            * ``βage2``: float, the parameter for the age^2 term.
+            * ``βDx2``: float, unused?
+            * ``βDx3``: float, unused?
+            * ``θ``: list of two numbers, which are used as the thresholds to compute the ordinal
+              regression.
+    """
+    def __init__(
+        self, config: dict | None = None,
+        parameters: dict | None = None,
+        hyperparameters: dict | None = None
+    ):
+        if config is None and parameters is None and hyperparameters is None:
+            raise ValueError(
+                "Either config dict or parameters and hyperparameters must be provided."
+            )
+        elif config is not None:
+            self.hyperparameters = config["hyperparameters"]
+            self.parameters = config["parameters"]
+            self.parameters["θ"] = np.array(self.parameters["θ"])
+            self.assign_random_β0()
+        else:
+            self.hyperparameters = hyperparameters
+            self.parameters = parameters
+            self.assign_random_β0()
+
+    def assign_random_β0(self):
+        """Assign the parameter β0 a random value from a normal distribution."""
+        self.parameters["β0"] = np.random.normal(
+            self.hyperparameters["β0_μ"],
+            self.hyperparameters["β0_σ"]
+        )
+
+    def compute_control_levels(self, sex: bool, age: int, initial: bool = False) -> dict:
+        """Compute the probability that the control level = k for each value of k.
+
+        The probability is given by ordinal regression, where y = control level:
+
+        .. math::
+
+            P(y <= k) = σ(θ_k - η)
+            P(y == k) = P(y <= k) - P(y < k + 1)
+                      = σ(θ_k - η) - σ(θ_(k+1) - η)
+
+        Args:
+            sex (bool): Sex of person, true = male, false = female.
+            age (int): The age of the person (agent) in years.
+            initial (bool): If this is the initial computation.
+
+        Returns:
+            ControlLevels: A ``ControlLevel`` object with the probability of each control level.
+                For example:
+                {
+                    "fully_controlled": 0.2,
+                    "partially_controlled": 0.75,
+                    "uncontrolled": 0.05
+                }
+
+        """
+        if initial:
+            age_scaled = (age - 1) / 100
+        else:
+            age_scaled = age / 100
+
+        η = (
+            self.parameters["β0"] +
+            age_scaled * self.parameters["βage"] +
+            sex * self.parameters["βsex"] +
+            age_scaled * sex * self.parameters["βsexage"] +
+            age_scaled ** 2 * sex * self.parameters["βsexage2"] +
+            age_scaled ** 2 * self.parameters["βage2"]
+        )
+
+        control_levels_prob = compute_ordinal_regression(η, self.parameters["θ"])
+        control_levels = ControlLevels(
+            fully_controlled=control_levels_prob[0],
+            partially_controlled=control_levels_prob[1],
+            uncontrolled=control_levels_prob[2]
+        )
+        return control_levels
--- leap/utils.py
+++ leap/utils.py
@@ -1,6 +1,44 @@
 import numpy as np
 import pathlib
+from typing import Callable
+
 
 LEAP_PATH = pathlib.Path(__file__).parents[1].absolute()
 PROCESSED_DATA_PATH = pathlib.Path(LEAP_PATH, "processed_data")
 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def compute_ordinal_regression(
+    η: float,
+    θ: float | list[float],
+    prob_function: Callable = sigmoid
+) -> list[float]:
+    """Compute the probability that y = k for each value of k.
+
+    The probability is given by ordinal regression:
+        P(y <= k) = σ(θ_k - η)
+        P(y == k) = P(y <= k) - P(y < k + 1)
+                  = σ(θ_k - η) - σ(θ_(k+1) - η)
+
+    Args:
+        η (float): the weight for the regression.
+        θ (float | list[float]): either a single value or an array of values for the
+            threshold parameter.
+        prob_function (function): A function to apply, default is the sigmoid function.
+
+    Returns:
+        list[float]: a vector with the probability of each value of k.
+            For example:
+            k=1 | k=2  | k=2
+            0.2 | 0.75 | 0.05
+    """
+
+    if isinstance(θ, float):
+        θ = [-10**5, θ, 10**5]
+    else:
+        θ = [-10**5] + θ + [10**5]
+
+    return [prob_function(θ[k + 1] - η) - prob_function(θ[k] - η) for k in range(len(θ) - 1)]
