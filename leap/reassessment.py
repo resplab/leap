effd0b41b955901c89cb1fbb3d496feeb27a0694
Ainsleigh <ainsleighhill@gmail.com>
2024-07-11 20:03:04 -0700

    add Reassessment class

---
+++ leap/reassessment.py
@@ -0,0 +1,85 @@
+import pathlib
+import pandas as pd
+import numpy as np
+from leap.utils import PROCESSED_DATA_PATH
+from leap.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class Reassessment:
+    """A class containing information about asthma diagnosis reassessment.
+
+    Attributes:
+        table (pd.api.typing.DataFrameGroupBy): A grouped data frame grouped by year.
+            Each data frame contains the following columns:
+                * ``year`` (int): calendar year.
+                * ``age`` (int): age of person.
+                * ``F`` (float): the probability that a female agent still has asthma.
+                * ``M`` (float): the probability that a male agent still has asthma.
+                * ``province`` (str): a string indicating the province abbreviation, e.g. "BC".
+                    For all of Canada, set province to "CA".
+            See ``master_asthma_reassessment.csv``.
+    """
+    def __init__(
+        self,
+        starting_year: int = 2000,
+        province: str = "CA",
+        table: pd.api.typing.DataFrameGroupBy | None = None
+    ):
+        if table is None:
+            self.table = self.load_reassessment_table(starting_year, province)
+        else:
+            self.table = table
+
+    def load_reassessment_table(
+        self, starting_year: int, province: str
+    ) -> pd.api.typing.DataFrameGroupBy:
+        """Load the asthma diagnosis reassessment table.
+
+        Args:
+            starting_year (int): the year to start the data at.
+            province (str): a string indicating the province abbreviation, e.g. "BC".
+                For all of Canada, set province to "CA".
+
+        Returns:
+            pd.api.typing.DataFrameGroupBy: A grouped data frame grouped by year.
+                Each data frame contains the following columns:
+                    * ``year`` (int): calendar year.
+                    * ``age`` (int): age of person.
+                    * ``F`` (float): the probability that a female agent still has asthma.
+                    * ``M`` (float): the probability that a male agent still has asthma.
+                    * ``province`` (str): a string indicating the province abbreviation, e.g. "BC".
+                        For all of Canada, set province to "CA".
+        """
+        df = pd.read_csv(
+            pathlib.Path(PROCESSED_DATA_PATH, "master_asthma_reassessment.csv")
+        )
+        df = df[
+            (df["year"] >= starting_year) &
+            (df["province"] == province)
+        ]
+        grouped_df = df.groupby(["year"])
+        return grouped_df
+
+    def agent_has_asthma(self, agent):
+        """If an agent has been diagnosed with asthma, determine whether the agent still has asthma.
+
+        Asthma is not curable, but it can be mistaken for other respiratory diseases, and so a
+        person could be diagnosed with asthma and later find this was a misdiagnosis. Additionally,
+        asthma can go into a period of dormancy, during which time the person does not have any
+        asthma symptoms.
+
+        Args:
+            agent (Agent): Agent module, see `Agent`.
+
+        """
+        max_year = len(self.table.groups.keys())
+        if agent.age < 4:
+            return agent.has_asthma
+        else:
+            df = self.table.get_group(min(agent.year_index, max_year))
+            df = df[df["age"] == agent.age]
+            sex = "F" if agent.sex == 0 else "M"
+            probability = df[sex].values[0]
+            return bool(np.random.binomial(1, probability))
