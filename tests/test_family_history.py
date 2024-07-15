7c84e2375bb664d50162c4920279182d1fdd8227
Ainsleigh <ainsleighhill@gmail.com>
2024-07-15 16:03:05 -0700

    add tests for family_history

---
+++ tests/test_family_history.py
@@ -0,0 +1,57 @@
+import pytest
+from leap.family_history import FamilyHistory
+
+
+@pytest.mark.parametrize(
+    "parameters, is_valid",
+    [
+        (
+            {
+                "p": 0.0,
+            },
+            True
+        ),
+        (
+            {
+                "p": 1.0,
+            },
+            True
+        ),
+        (
+            {
+                "p": 0.5,
+            },
+            True
+        ),
+        (
+            {
+                "p": 5.6,
+            },
+            False
+        ),
+    ]
+)
+def test_family_history_constructor(parameters, is_valid):
+    if is_valid:
+        family_history = FamilyHistory(parameters=parameters)
+        assert family_history.parameters["p"] == parameters["p"]
+    else:
+        with pytest.raises(ValueError) as error:
+            family_history = FamilyHistory(parameters=parameters)
+        assert str(error.value) == "p must be a probability between 0 and 1, received 5.6."
+
+
+@pytest.mark.parametrize(
+    "parameters, has_family_history",
+    [
+        (
+            {
+                "p": 0.0,
+            },
+            False
+        )
+    ]
+)
+def test_family_history_has_family_history_of_asthma(parameters, has_family_history):
+    family_history = FamilyHistory(parameters=parameters)
+    assert family_history.has_family_history_of_asthma() == has_family_history
