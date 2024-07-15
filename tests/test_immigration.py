1fb4f9827ab894e9f4bac747abe86d74703e32df
Ainsleigh <ainsleighhill@gmail.com>
2024-07-15 16:45:12 -0700

    add tests for immigration

---
+++ tests/test_immigration.py
@@ -0,0 +1,78 @@
+import pytest
+from leap.immigration import Immigration
+from leap.utils import round_number
+
+
+@pytest.mark.parametrize(
+    (
+        "starting_year, year, age, max_age, sex, province, population_growth_type,"
+        "n_prop_birth, weights"
+    ),
+    [
+        (
+            2024,
+            2026,
+            4,
+            111,
+            0,
+            "BC",
+            "LG",
+            0.004980,
+            0.004936
+        ),
+        (
+            2024,
+            2025,
+            4,
+            111,
+            1,
+            "BC",
+            "LG",
+            0.007383,
+            0.007294
+        ),
+    ]
+)
+def test_immigration_constructor(
+    starting_year, year, age, max_age, sex, province, population_growth_type, n_prop_birth, weights
+):
+    immigration = Immigration(
+        starting_year=starting_year,
+        province=province,
+        population_growth_type=population_growth_type,
+        max_age=max_age
+    )
+    df = immigration.table.get_group((year))
+    row = df[(df["age"] == age) & (df["sex"] == sex)]
+    assert round_number(row["n_prop_birth"].values[0], sigdigits=4) == n_prop_birth
+    assert round_number(row["weights"].values[0], sigdigits=4) == weights
+
+
+@pytest.mark.parametrize(
+    (
+        "starting_year, year, max_age, province, population_growth_type,"
+        "num_new_born, num_new_immigrants"
+    ),
+    [
+        (
+            2024,
+            2025,
+            111,
+            "BC",
+            "LG",
+            1000,
+            1013
+        )
+    ]
+)
+def test_immigration_get_num_new_immigrants(
+    starting_year, year, max_age, province, population_growth_type, num_new_born,
+    num_new_immigrants
+):
+    immigration = Immigration(
+        starting_year=starting_year,
+        province=province,
+        population_growth_type=population_growth_type,
+        max_age=max_age
+    )
+    assert immigration.get_num_new_immigrants(num_new_born, year) == num_new_immigrants
