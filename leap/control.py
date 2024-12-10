276a3aaa945fbf6cfa389f62970597be8dbc4fa7
Ainsleigh <ainsleighhill@gmail.com>
2024-12-10 15:18:59 -0800

    modify control

--- leap/control.py
+++ leap/control.py
@@ -29,28 +29,12 @@ class ControlLevels:
 class Control:
     """A class containing information about asthma control.
 
-    This refers to how well the condition is managed.
-    There are three levels of asthma control:
-        fully-controlled = 1
-        partially-controlled = 2
-        uncontrolled = 3
+    This refers to how well the condition is managed. There are three levels of asthma control:
 
-    Attributes:
-        hyperparameters (dict): A dictionary containing the hyperparameters used
-            to compute `β0` from a normal distribution:
-            * ``β0_μ``: float, the mean of the normal distribution.
-            * ``β0_σ``: float, the standard deviation of the normal distribution.
-        parameters (dict): A dictionary containing the following keys:
-            * ``β0``: float, a constant parameter. See `hyperparameters`.
-            * ``βage``: float, the parameter for the age term.
-            * ``βsex``: float, the parameter for the sex term.
-            * ``βsexage``: float, the parameter for the sex * age term.
-            * ``βsexage2``: float, the parameter for the sex * age^2 term.
-            * ``βage2``: float, the parameter for the age^2 term.
-            * ``βDx2``: float, unused?
-            * ``βDx3``: float, unused?
-            * ``θ``: list of two numbers, which are used as the thresholds to compute the ordinal
-              regression.
+        * 1 = fully-controlled
+        * 2 = partially-controlled
+        * 3 = uncontrolled
+
     """
     def __init__(
         self,
@@ -71,6 +55,46 @@ class Control:
             raise ValueError(
                 "Either config dict or parameters and hyperparameters must be provided."
             )
+        
+    @property
+    def hyperparameters(self) -> dict:
+        """Hyperparameters used to compute ``β0`` from a normal distribution:
+            * ``β0_μ``: float, the mean of the normal distribution.
+            * ``β0_σ``: float, the standard deviation of the normal distribution.
+        """
+        return self._hyperparameters
+    
+    @hyperparameters.setter
+    def hyperparameters(self, hyperparameters: dict):
+        KEYS = ["β0_μ", "β0_σ"]
+        for key in KEYS:
+            if key not in hyperparameters:
+                raise ValueError(f"Missing key {key} in hyperparameters.")
+        self._hyperparameters = hyperparameters
+
+    @property
+    def parameters(self) -> dict:
+        """A dictionary containing the following keys:
+            * ``β0``: float, a constant parameter. See ``hyperparameters``.
+            * ``βage``: float, the parameter for the ``age`` term.
+            * ``βsex``: float, the parameter for the ``sex`` term.
+            * ``βsexage``: float, the parameter for the ``sex * age`` term.
+            * ``βsexage2``: float, the parameter for the ``sex * age^2`` term.
+            * ``βage2``: float, the parameter for the ``age^2`` term.
+            * ``βDx2``: float, unused?
+            * ``βDx3``: float, unused?
+            * ``θ``: list of two numbers, which are used as the thresholds to compute the ordinal
+              regression.
+        """
+        return self._parameters
+    
+    @parameters.setter
+    def parameters(self, parameters: dict):
+        KEYS = ["βage", "βsex", "βsexage", "βsexage2", "βage2", "βDx2", "βDx3", "θ"]
+        for key in KEYS:
+            if key not in parameters:
+                raise ValueError(f"Missing key {key} in parameters.")
+        self._parameters = parameters
 
     def assign_random_β0(self):
         """Assign the parameter β0 a random value from a normal distribution."""
@@ -79,8 +103,10 @@ class Control:
             self.hyperparameters["β0_σ"]
         )
 
-    def compute_control_levels(self, sex: Sex, age: int, initial: bool = False) -> ControlLevels:
-        """Compute the probability that the control level = k for each value of k.
+    def compute_control_levels(
+        self, sex: Sex | int, age: int, initial: bool = False
+    ) -> ControlLevels:
+        """Compute the probability that the control level = ``k`` for each value of ``k``.
 
         The probability is given by ordinal regression, where y = control level:
 
@@ -93,16 +119,29 @@ class Control:
         Args:
             sex: Sex of person, 1 = male, 0 = female.
             age: The age of the person (agent) in years.
-            initial: If this is the initial computation.
+            initial: Whether or not this is the initial computation.
 
         Returns:
             ControlLevels: A ``ControlLevel`` object with the probability of each control level.
-                For example:
-                {
-                    "fully_controlled": 0.2,
-                    "partially_controlled": 0.75,
-                    "uncontrolled": 0.05
-                }
+
+        Examples:
+
+            >>> from leap.control import Control
+            >>> from leap.utils import get_data_path
+            >>> import json
+            >>> with open(get_data_path("processed_data", "config.json")) as f:
+            ...     config = json.load(f)["control"]
+            >>> config["parameters"]["θ"] = [-10**5, -10**5] 
+            >>> control = Control(config=config)
+            >>> control_levels = control.compute_control_levels(sex=0, age=45)
+            >>> control_levels.as_array()
+            array([0., 0., 1.])
+            >>> control_levels.fully_controlled
+            np.float64(0.0)
+            >>> control_levels.partially_controlled
+            np.float64(0.0)
+            >>> control_levels.uncontrolled
+            np.float64(1.0)
 
         """
         if initial:
