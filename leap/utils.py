ecee45a1d6d52bd2dc0e47a9418b2d3f94a5b343
Ainsleigh <ainsleighhill@gmail.com>
2025-01-05 16:43:34 -0800

    update get_data_path

--- leap/utils.py
+++ leap/utils.py
@@ -119,7 +119,7 @@ def get_data_path(data_path: str) -> pathlib.Path:
     elif pathlib.Path(data_path).parts[0] == "processed_data":
         data_path = str(pathlib.Path(data_path).relative_to("processed_data"))
 
-    for data_folder in ["tests.data", "processed_data"]:
+    for data_folder in ["tests.data", "leap.processed_data"]:
         try:
             package_path = str(pkg_resources.files(data_folder).joinpath(data_path))
             if os.path.isfile(package_path) or os.path.isdir(package_path):
