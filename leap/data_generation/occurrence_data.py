1c2677a36aaae0262ba11e1646f46a81da90d8ad
Ainsleigh <ainsleighhill@gmail.com>
2025-03-03 16:20:18 -0800

    add code to generate occurrence models

--- leap/data_generation/occurrence_data.py
+++ leap/data_generation/occurrence_data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from leap.utils import get_data_path
 from leap.logger import get_logger
-from typing import Tuple
+from typing import Tuple, TYPE_CHECKING
+if TYPE_CHECKING:
+    from statsmodels.genmod.generalized_linear_model import GLMResultsWrapper
 
 pd.options.mode.copy_on_write = True
 
@@ -91,8 +95,109 @@ def load_asthma_df(starting_year: int = STARTING_YEAR) -> pd.DataFrame:
     return df
 
 
+def generate_occurrence_model(
+    df_asthma: pd.DataFrame, formula: str, occ_type: str, maxiter: int = 1000
+) -> GLMResultsWrapper:
+    """Generate a ``GLM`` model for asthma incidence or prevalence.
+    
+    Args:
+        df_asthma: The asthma dataframe. Must have columns:
+
+            * ``year (int)``: The calendar year.
+            * ``sex (str)``: One of ``M`` = male, ``F`` = female.
+            * ``age (int)``: The age in years.
+            * ``incidence (float)``: The incidence of asthma.
+            * ``prevalence (float)``: The prevalence of asthma.
+            
+        formula: The formula for the GLM model. See the ```statsmodels`` documentation 
+            <https://www.statsmodels.org/stable/examples/notebooks/generated/glm_formula.html>`_
+            for more information.
+        occ_type: The type of occurrence data to model. Must be one of ``"incidence"`` or
+            ``"prevalence"``.
+        maxiter: The maximum number of iterations to perform while fitting the model.
+    
+    Returns:
+        The fitted ``GLM`` model.
+    """
+
+    # Create occurrence dataframe
+    df = df_asthma[["year", "sex", "age", occ_type]].copy()
+
+    # Convert sex string to 0 or 1
+    df["sex"] = df.apply(
+        lambda x: 0 if x["sex"] == "F" else 1,
+        axis=1
+    )
+
+    # Fit the GLM model
+    model = smf.glm(formula=formula, data=df, family=sm.families.Gaussian())
+    results = model.fit(maxiter=maxiter)
+
+    print(results.summary())
+
+    return results
+
+
+def generate_incidence_model(
+    df_asthma: pd.DataFrame, maxiter: int = 1000
+) -> GLMResultsWrapper:
+    """Generate a ``GLM`` model for asthma incidence.
+    
+    Args:
+    
+        df_asthma: The asthma dataframe. Must have columns:
+
+            * ``year (int)``: The calendar year.
+            * ``sex (str)``: One of ``M`` = male, ``F`` = female.
+            * ``age (int)``: The age in years.
+            * ``incidence (float)``: The incidence of asthma.
+            * ``prevalence (float)``: The prevalence of asthma.
+
+        maxiter: The maximum number of iterations to perform while fitting the model.
+    
+    Returns:
+        The fitted ``GLM`` model.
+    """
+
+    formula = "np.log(incidence) ~ sex*year + sex*poly(age, degree=5)"
+    results = generate_occurrence_model(
+        df_asthma, formula=formula, occ_type="incidence", maxiter=maxiter
+    )
+    return results
+
+
+def generate_prevalence_model(
+    df_asthma: pd.DataFrame, maxiter: int = 1000
+) -> GLMResultsWrapper:
+    """Generate a ``GLM`` model for asthma prevalence.
+    
+    Args:
+    
+        df_asthma: The asthma dataframe. Must have columns:
+
+            * ``year (int)``: The calendar year.
+            * ``sex (str)``: One of ``M`` = male, ``F`` = female.
+            * ``age (int)``: The age in years.
+            * ``incidence (float)``: The incidence of asthma.
+            * ``prevalence (float)``: The prevalence of asthma.
+        
+        maxiter: The maximum number of iterations to perform while fitting the model.
+    
+    Returns:
+        The fitted ``GLM`` model.
+    """
+
+    formula = "np.log(prevalence) ~ sex*poly(year, degree=2)*poly(age, degree=5)"
+    results = generate_occurrence_model(
+        df_asthma, formula=formula, occ_type="prevalence", maxiter=maxiter
+    )
+    return results
+
+
 def generate_occurrence_data():
     df_asthma = load_asthma_df()
+    incidence_model = generate_incidence_model(df_asthma)
+    prevalence_model = generate_prevalence_model(df_asthma)
 
 
 if __name__ == "__main__":
