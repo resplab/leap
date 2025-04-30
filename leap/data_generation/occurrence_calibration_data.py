9b912f5ac7baa9d64f8375edfcad1102e32acb3b
Ainsleigh <ainsleighhill@gmail.com>
2025-04-30 16:23:44 -0700

    add load_occurrence_data function

--- leap/data_generation/occurrence_calibration_data.py
+++ leap/data_generation/occurrence_calibration_data.py
@@ -1,6 +1,7 @@
 import pathlib
 import pandas as pd
 import numpy as np
+import itertools
 from leap.utils import get_data_path
 from leap.logger import get_logger
 from typing import Tuple
@@ -55,6 +56,49 @@ def asthma_predictor(age: int, sex: str, year: int, occurrence_type: str) -> flo
         (DF_OCC_PRED["year"] == year) &
         (DF_OCC_PRED["sex"] == sex)
     ][occurrence_type].values[0]
+
+
+def load_occurrence_data(
+    province: str = PROVINCE,
+    min_year: int = MIN_YEAR,
+    max_year: int = MAX_YEAR
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Load the asthma incidence and prevalence data for the given province and year range.
+
+    Args:
+        province: The province to load data for.
+        min_year: The minimum year to load data for.
+        max_year: The maximum year to load data for.
+
+    Returns:
+        A tuple of two DataFrames: the first is the incidence data, and the second is the
+        prevalence data.
+    """
+
+    df_asthma = pd.DataFrame(
+        list(itertools.product(range(3, 111), [0, 1], range(min_year, max_year + 1))),
+        columns=["age", "sex", "year"]
+    )
+
+    df_asthma["incidence"] = df_asthma.apply(
+        lambda x: asthma_predictor(x["age"], x["sex"], x["year"], "inc"),
+        axis=1
+    )
+    df_asthma["prevalence"] = df_asthma.apply(
+        lambda x: asthma_predictor(x["age"], x["sex"], x["year"], "prev"),
+        axis=1
+    )
+    df_asthma["incidence"] = df_asthma.apply(
+        lambda x: x["prevalence"] if x["age"] == 3 else x["incidence"],
+        axis=1
+    )
+    df_asthma["province"] = [province] * df_asthma.shape[0]
+
+    df_incidence = df_asthma[["year", "age", "sex", "incidence"]].copy()
+    df_prevalence = df_asthma[["year", "age", "sex", "prevalence"]].copy()
+
+    return df_incidence, df_prevalence
+
 
     
 
