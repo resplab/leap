7e7569df47d80c6473dd783c1b3d2aef94e2a294
Ainsleigh <ainsleighhill@gmail.com>
2025-05-04 14:20:27 -0700

    move compute_contingency_tables to incidence_calibration

--- leap/data_generation/occurrence_calibration_data.py
+++ leap/data_generation/occurrence_calibration_data.py
@@ -4,7 +4,7 @@ from statsmodels.genmod.generalized_linear_model import GLMResultsWrapper
 from scipy import optimize
 import itertools
 from leap.utils import get_data_path
-from leap.data_generation.utils import heaviside, conv_2x2
+from leap.data_generation.utils import heaviside
 from leap.data_generation.antibiotic_data import get_predicted_abx_data, generate_antibiotic_data
 from leap.logger import get_logger
 from typing import Tuple
@@ -894,78 +894,4 @@ def prev_calibrator(
     return res.x
 
 
-
-def compute_contingency_table(
-    risk_factor_prev: list[float],
-    odds_ratio_target: list[float],
-    asthma_prev_calibrated: list[float]
-) -> list[pd.DataFrame]:
-    r"""Compute the contingency table for the risk factors and asthma prevalence.
-
-    This function computes the proportions of the population at different levels of family
-    history and antibiotic exposure. The proportions are calculated based on the risk factors
-    and the calibrated asthma prevalence. The function uses the ``metafor`` package to convert
-    odds ratios into proportions.
-
-    See: Bonett, D. G. (2007). Transforming odds ratios into correlations for
-    meta-analytic research. American Psychologist, 62(3), 254-255.
-    https://doi.org/10.1037/0003-066x.62.3.254
-
-    Args:
-        risk_factor_prev: A vector of the prevalence of the risk factor levels.
-        odds_ratio_target: A vector of odds ratios for the risk factors.
-        asthma_prev_calibrated: A vector of the calibrated asthma prevalence.
-
-    Returns:
-        A list of vectors representing the proportions of the population for different risk
-        factor levels / combinations. For example, if we have the following risk factors:
-
-        * family history: ``{0, 1}``
-        * antibiotic exposure: ``{0, 1, 2, 3}``
-
-        then we have ``2 * 4 = 8`` combinations. Each combination is called a ``risk factor level``,
-        and is indexed by ``i`` (this corresponds to the index in the risk_set table). The first
-        combination, ``i = 1`` is a special case; this is where there are no risk factors. We use
-        this combination, referred to as the ``ref`` level, in the calculation of all the tables.
-        
-        Each list entry contains a Pandas dataframe, with the following entries:
-
-            * ``di``: proportion of population labelled as no asthma with no risk factors
-            * ``ci``: proportion of population labelled as asthma with no risk factors
-            * ``bi``: proportion of population labelled as no asthma with risk factors
-            * ``ai``: proportion of population labelled as asthma with risk factors
-    """
-
-    contingency_tables = []
-
-    asthma_prev_ref = asthma_prev_calibrated[0]
-    risk_factor_prev_ref = risk_factor_prev[0]
-
-    for i in range(len(odds_ratio_target)):
-        risk_factor_prev_i = risk_factor_prev[i]
-        asthma_prev = asthma_prev_calibrated[i]
-
-        # prevalence of risk factor combination i
-        risk_factor_prev_i = risk_factor_prev_i / (risk_factor_prev_i + risk_factor_prev_ref)
-
-        sample_size = 1e10
-        # prevalence of risk factor combination i
-        n1i = sample_size * risk_factor_prev_i
-        # prevalence of asthma
-        n2i = sample_size * np.dot(
-            [1 - risk_factor_prev_i, risk_factor_prev_i],
-            [asthma_prev_ref, asthma_prev]
-        )
-        table = conv_2x2(
-            ori=odds_ratio_target[i],
-            ni=sample_size,
-            n1i=n1i,
-            n2i=n2i
-        )
-
-        # divide by sample size to get proportions
-        table["values"] = table["values"].apply(lambda x: x / sample_size)
-
-        contingency_tables.append(table)
-
-    return contingency_tables
+    
